f92263ff7cabc028f2fcb659c5a3b699b399eb27
kapil-2025 <kapilguptakapil1111@gmail.com>
2025-06-21 04:25:16 -0700

    add object oriented programming

---
+++ oops/01_basic_class_and_object.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self,brand,model):
+        self.kapil=model
+        self.himank=brand
+
+        my_car=Car("tata","ferrari")
+        print(my_car.brand)
+
+
+        
